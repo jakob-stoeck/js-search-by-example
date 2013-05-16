41729067d0f89359f235334942e653380cbd6061
Jakob Stoeck <jakob@pokermania.de>
2013-05-16 23:04:54 +0200

    grouped by static and instance functions

--- scripts/search.js
+++ scripts/search.js
@@ -1,5 +1,14 @@
+(function() {
 'use strict';
-var searchByExample = {
+var s = new String()
+  , n = new Number()
+  , a = new Array()
+  , equals = function(a, b) {
+    return typeof a === 'object' || typeof b === 'object'
+        ? _.isEqual(a, b)
+        : a == b;
+  }
+  , searchByExample = {
   getArgs: function() {
     return {
       input: arguments[0],
@@ -8,53 +17,82 @@ var searchByExample = {
       object: typeof arguments[0] == 'string' ? new String() : new Number()
     };
   },
-  objectInstanceFunctions: [
-    // string
-    'toUpperCase',
-    'toLowerCase',
-    'toLocaleUpperCase',
-    'toLocaleLowerCase',
-    'valueOf',
-    'concat',
-    'match',
-    'search',
-    'charCodeAt',
-    'split',
-    'replace',
-    // number
-    'toFixed',
-    'toString',
-    'toPrecision',
-    'toExponential',
-    'toLocaleString'
+  instanceFun: [
+    s.toUpperCase,
+    s.toLowerCase,
+    s.toLocaleUpperCase,
+    s.toLocaleLowerCase,
+    s.valueOf,
+    s.concat,
+    s.match,
+    s.search,
+    s.charCodeAt,
+    s.split,
+    s.replace,
+    n.toFixed,
+    n.toString,
+    n.toPrecision,
+    n.toExponential,
+    n.toLocaleString,
+    a.join,
+    a.isArray,
+    a.slice
   ],
-  isRight: function(fun) {
+  staticFun: [
+    String.fromCharCode,
+    Math.abs,
+    Math.cos,
+    Math.exp,
+    Math.log,
+    Math.max,
+    Math.min,
+    Math.pow,
+    Math.sin,
+    Math.tan,
+    Math.ceil,
+    Math.sqrt,
+    Math.floor,
+    Math.round,
+    Object.keys
+  ],
+  // TODO by reference functions Array.push etc.
+  tryInstance: function(fun) {
     try {
-      var ret = this.object[fun].apply(this.input, this.args);
-      return typeof ret === 'object' || typeof this.output === 'object'
-        ? _.isEqual(ret, this.output)
-        : ret == this.output;
-    } catch (e) {
-      // console.warn(e + '.  fun: ' + fun);
-    }
+      var ret = fun.apply(this.input, this.args);
+      return equals(ret, this.output);
+    } catch (e) {}
+  },
+  tryStatic: function(fun) {
+    try {
+      var ret = fun.apply(this, this.args);
+      return equals(ret, this.output);
+    } catch (e) {}
   },
   find: function(args) {
-    return this.objectInstanceFunctions.filter(this.isRight, args);
+    args = this.getArgs.apply(this, args);
+    var inst = this.instanceFun.filter(this.tryInstance, args);
+    args.args.unshift(args.input);
+    var stat = this.staticFun.filter(this.tryStatic, args);
+    return inst.concat(stat);
   }
-};
-
-var onEnter = function (event) {
+}
+  , onEnter = function (event) {
   if (event.keyCode === 13) {
+    event.preventDefault();
     var input = event.target.value;
     var output = '';
     try {
-      var clean = input.split(';').filter(function(x) { return x; }).map(eval);
-      var args = searchByExample.getArgs.apply(this, clean);
-      output = searchByExample.find(args);
-    } catch (e) {
+      var clean = input.split(';')
+        .filter(function(x) { return x; })
+        .map(function(el) { return eval('('+el+')'); });
+      output = searchByExample.find(clean).join('\n');
+    }
+    catch (e) {
+      // show syntax errors in output window
       output = e;
     }
     document.getElementById('methods').value = output;
-    event.preventDefault();
   }
-}
+};
+document.getElementById('example').onkeydown = onEnter;
+})();
